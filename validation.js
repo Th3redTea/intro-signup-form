98b0667286d8df766ca165a6bd33be3cc648668c
Amine Amhoume <amhoume@gmail.com>
2021-06-24 18:52:26 +0100

    fixed scss

---
+++ validation.js
@@ -0,0 +1,10 @@
+const firstValid = document.getElementById('firstName')
+const nameValid = document.getElementById('firstName')
+const emailValid = document.getElementById('firstName')
+const passwordValid = document.getElementById('firstName')
+
+let firstLabel = document.createElement('label')
+let nameLabel = document.createElement('label')
+let emailLabel = document.createElement('label')
+let passwordLabel = document.createElement('label')
+
